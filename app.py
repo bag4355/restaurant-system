03b9e930eb458517c10046fcc9f0bbc11409750b
bag4355 <bag4355@yonsei.ac.kr>
2025-04-23 01:52:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ KITCHEN_PW = os.getenv("KITCHEN_PW", "kitchen123")
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "3306")
-DB_NAME = os.getenv("DB_NAME", "aifrestaurant")
-DB_USER = os.getenv("DB_USER", "appuser")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS", "A1b2C3d4!")
 
 DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
