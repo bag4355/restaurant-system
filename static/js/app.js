ba1d504518999c60c1781d97b50991aa5b96a912
bag4355 <bag4355@yonsei.ac.kr>
2025-05-29 15:17:09 +0900

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const toastElList = [].slice.call(document.querySelectorAll('.toast'));
-  
+
   // 각 토스트를 순차적으로(2초 간격) 나타내고, 3초 후 autohide
   toastElList.forEach((toastEl, index) => {
     const delayBeforeShow = 2000 * index; // 이전 것과 2초 간격
